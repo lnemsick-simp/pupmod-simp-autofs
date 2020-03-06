ccf93dc44e4174c9513b1063f630781fb01ab9b9
lnemsick-simp <lnemsick.simp@gmail.com>
2020-03-06 15:27:48 -0500

    dd install and service tests to init test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -10,6 +10,23 @@ describe 'autofs' do
       it { is_expected.to contain_class('autofs::install') }
       it { is_expected.to contain_class('autofs::config') }
       it { is_expected.to contain_class('autofs::service') }
+
+      # autofs::install class is trivial, so test it here
+      it { is_expected.to contain_package('samba-client').with_ensure('installed') }
+      it { is_expected.to contain_package('autofs').with_ensure('installed') }
+
+      # autofs::class class is also trivial, so test it here
+      it { is_expected.to contain_service('autofs').with( {
+       :ensure     => 'running',
+       :enable     => true,
+       :hasstatus  => true,
+       :hasrestart => true
+      } ) }
+
+      it { is_expected.to contain_exec('autofs_reload').with( {
+        :command     => '/usr/bin/systemctl reload autofs',
+        :refreshonly => true
+      } ) }
     end
   end
 end
